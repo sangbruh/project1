4d5252874eb923bb96efa9b06163b001c59cc01b
sang <29sangu@gmail.com>
2023-10-23 11:01:34 +0200

    Added Docker install and set up in script1

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
+
+# Install and set up Docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker $USER  # Replace with your system's username, e.g., 'ubuntu'
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
