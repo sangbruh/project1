cfaa46a7a93830e5ca880e06206f3729bc19383c
sang <29sangu@gmail.com>
2023-10-24 17:42:59 +0200

    Added new script4

---
+++ scripts/script4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Searches for package updates and installs them
+sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
+
+# Create user for Prometheus and install Prometheus
+sudo useradd --system --no-create-home --shell /bin/false prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.47.1/prometheus-2.47.1.linux-amd64.tar.gz
+
+# Unzip and prepare Prometheus files
+tar -xvf prometheus-2.47.1.linux-amd64.tar.gz
+cd prometheus-2.47.1.linux-amd64/
+sudo mkdir -p /data /etc/prometheus
+sudo mv prometheus promtool /usr/local/bin/
+sudo mv consoles/ console_libraries/ /etc/prometheus/
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
