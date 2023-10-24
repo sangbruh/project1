1ccdbc706c7303c53d7b3593443a6e762efb90cc
sang <29sangu@gmail.com>
2023-10-24 18:20:28 +0200

    Added new script5

--- terraform/main.tf
+++ terraform/main.tf
@@ -151,6 +151,15 @@ resource "aws_security_group" "M_SG" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "Prometheus"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
