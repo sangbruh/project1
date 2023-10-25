d1182aba6619ae0b08201e641043fee1fc3f161c
sang <29sangu@gmail.com>
2023-10-25 15:56:18 +0200

    Added new TF file for project1 on Azure

---
+++ azTF/main.tf
@@ -0,0 +1,190 @@
+provider "azurerm" {
+}
+
+/*resource "aws_key_pair" "netflix-clone-key" {
+  key_name   = "netflix-clone-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "netflix-clone-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
+}
+
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-0fb46171d561011c8"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Docker"
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SonarQube"
+    from_port        = 9000
+    to_port          = 9000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
+
+resource "aws_instance" "web1" {
+  ami           = "ami-0fe8bec493a81c7da"
+  instance_type = "t3.large"
+  key_name = "netflix-clone-key"
+  security_groups = [aws_security_group.TF_SG.name]
+  ebs_block_device {
+    volume_size = 25
+    device_name = "/dev/sda1"
+  }
+
+  tags = {
+    Name = "Netflix-clone-jenkins"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0fe8bec493a81c7da"
+  instance_type = "t3.medium"
+  key_name = "netflix-clone-key"
+  security_groups = [aws_security_group.M_SG.name]
+  ebs_block_device {
+    volume_size = 20
+    device_name = "/dev/sda1"
+  }
+
+  tags = {
+    Name = "Monitoring"
+  }
+}
+
+resource "aws_security_group" "M_SG" {
+  name        = "security group for monitoring"
+  description = "security group for monitoring"
+  vpc_id      = "vpc-0fb46171d561011c8"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Prometheus"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    description      = "Grafana"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "M_SG"
+  }
+}
+
+resource "aws_eip" "elasticip1" {
+  instance = aws_instance.web1.id
+}
+
+resource "aws_eip" "elasticip2" {
+  instance = aws_instance.web2.id
+} */
