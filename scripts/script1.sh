0de520922359f4548f480195855fcfb8b5b71b4f
sang <29sangu@gmail.com>
2023-10-27 22:28:07 +0200

    Edited code

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -6,5 +6,4 @@ sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER  # Replace with your system's username, e.g., 'ubuntu'
-newgrp docker
 sudo chmod 777 /var/run/docker.sock
